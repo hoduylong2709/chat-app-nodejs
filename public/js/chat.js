d6be6df5ed285d48dd22c24be17da55de0713941
long ho <long_ho@datahouse.com>
2021-09-24 21:58:05 +0700

    Commit - 24.09.2021

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,13 +13,19 @@ const locationMessageTemplate = document.querySelector('#location-message-templa
 
 socket.on('message', (message) => {
   console.log(message);
-  const html = Mustache.render(messageTemplate, { message });
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm a')
+  });
   $messages.insertAdjacentHTML('beforeend', html);
 });
 
-socket.on('locationMessage', (url) => {
-  console.log(url);
-  const html = Mustache.render(locationMessageTemplate, { url });
+socket.on('locationMessage', (message) => {
+  console.log(message);
+  const html = Mustache.render(locationMessageTemplate, {
+    message: message.url,
+    createdAt: moment(message.createdAt).format('h:mm a')
+  });
   $messages.insertAdjacentHTML('beforeend', html);
 });
 
